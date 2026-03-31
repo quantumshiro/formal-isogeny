c9a8ec3c7b3ecf6620ebbd8812e8159e6f6f8211
quantumshiro <hiro.1nakanishi@gmail.com>
2026-03-31 23:19:48 +0900

    Implement WeierstrassCurveMorphism structure and related imports

--- FormalIsogeny.lean
+++ FormalIsogeny.lean
@@ -1,1 +1,26 @@
-import FormalIsogeny.Basic
+import Mathlib.AlgebraicGeometry.EllipticCurve.Weierstrass
+import Mathlib.AlgebraicGeometry.EllipticCurve.Projective.Point
+import Mathlib.Algebra.Group.Subgroup.Basic
+
+namespace FormalIsogeny
+universe u
+
+variable {F : Type u} [Field F]
+
+-- Geometrically morphism
+-- is_regular is implåemented as homomogeneous formula
+structure WeierstrassCurveMorphism (W_1 W_2 : WeierstrassCurve F)
+where
+  toFun : W_1.toProjective.Point → W_2.toProjective.Point
+  is_regular : Prop
+
+instance {W_1 W_2 : WeierstrassCurve F} :
+  CoeFun (WeierstrassCurveMorphism (F := F) W_1 W_2)
+  (
+    fun _ =>
+      W_1.toProjective.Point →
+      W_2.toProjective.Point
+  ) where
+  coe φ := φ.toFun
+
+end FormalIsogeny
